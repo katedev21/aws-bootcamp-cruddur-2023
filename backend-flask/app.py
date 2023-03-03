30a8b69b4d42ab5ca2f65c078e4d8a867cdfc252
KateDev <kateho2011@gmail.com>
2023-03-03 22:38:11 +0000

    xray subsegment

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -106,10 +106,13 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
-@app.route("/api/activities/notifications", methods=['GET'])
-def data_notifications():
-  data = NotificationsActivities.run()
-  return data, 200
+@app.route("/api/activities/@<string:handle>", methods=['GET'])
+def data_handle(handle):
+  model = UserActivities.run(handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
 @app.route("/api/activities/search", methods=['GET'])
 def data_search():
