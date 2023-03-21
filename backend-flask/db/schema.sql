e5945e80dd79badb37d7cdb802a6b7e592bc119b
KateDev <kateho2011@gmail.com>
2023-03-21 02:50:05 +0000

    Create Congito Trigger to insert user into database

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE public.users (
   uuid UUID default uuid_generate_v4() primary key,
   display_name text,
   handle text,
-  cognito_user_id text,
+  email text NOT NULL,
+  cognito_user_id text NOT NULL,
   created_at timestamp default current_timestamp NOT NULL
 );
 
